add80a8951b316cb3ee18d45c48a05f83d08a1f5
boringthegod <pierre.ceberio@ynov.com>
2023-05-13 23:02:05 +0200

    add pip3 installation

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requirements = [x.strip() for x in open("requirements.txt", "r").readlines()]
+requirements = [
+    f"{line.split('#egg=')[-1]} @ {line}" if "#egg=" in line else line for line in requirements]
+
+setup(
+    name='postmaniac',
+    version='0.7.0',
+    packages=find_packages(),
+    license='GNU General Public License v3 (GPLv3)',
+    license_files=('LICENSE.md'),
+    author='boringthegod',
+    author_email='boringthegod@tutanota.com',
+    description='Postman OSINT tool to extract creds, token, username, email & more from Postman Public Workspaces.',
+    url='https://github.com/boringthegod/postmaniac',
+    keywords=["osint", "pentest", "cybersecurity",
+              "investigation", "lespireshat", "postman"],
+    entry_points={
+        'console_scripts': [
+            'postmaniac=postmaniac.postmaniac:main'
+        ]
+    },
+    install_requires=requirements
+)
